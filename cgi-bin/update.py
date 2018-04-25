4ad3c825b4a061da0b34daec4748e169788b8be1
matthewpdias <matthewpdias@gmail.com>
2018-04-25 15:02:25 -0400

    it mostly works, need to document configs and the rest

---
+++ cgi-bin/update.py
@@ -0,0 +1,32 @@
+#!/usr/local/bin/python3
+
+import os
+import boto3
+import yaml
+
+#setup s3 client
+s3 = boto3.resource('s3')
+
+#set the name of the bucket using the name of the current dir. This requires the dir name EXACTLY MATCHES THE BUCKET NAME
+dirname = os.path.basename(os.getcwd())
+
+#supported file mime types so content is presented correctly
+mime_types = { ".txt":"text/plain", ".html":"text/html", ".jpg" : "image/jpeg", ".png" : "image/png", ".mpeg" : "audio/mpeg", ".ogg" : "audio/ogg", ".mp4" : "video/mp4", ".json" : "application/json", ".js" : "application/javascript" }
+
+#get the files in the current dir. Currently only supports "flat" dirs with no subdirs
+for filename in os.listdir():
+
+    try:
+        mime = '.' + filename.split('.', 1)[-1]
+    except:
+        print('WARNING: Unsupported file extension: %s', filename.split('.', 1)[-1])
+        print('WARNING: %s will not be uploaded..', filename)
+        mime = '.'
+
+    #do not upload any dot files (like .git) or nonsupported files
+    if (filename[0] == '.' or mime == '.'):
+        pass
+    else:
+        #open and upload the current file to s3
+        with open(filename, 'rb') as data:
+            s3.Bucket(dirname).put_object(Key=filename, Body=data, ACL='public-read', ContentType=mime_types[mime])
---
+++ run-cgi.py
@@ -0,0 +1,50 @@
+#!/usr/local/bin/python3
+
+import subprocess
+import signal
+import time
+import os
+
+
+cwd = os.getcwd()
+
+def add_button():
+    try:
+        with open("index.html", 'a') as file:
+            file.write('<a href="cgi-bin/update.py"> publish </a>')
+    except:
+        print("no index.html file found, shutting server down")
+        exit(1)
+
+def trim_button():
+    readFile = open("index.html")
+    lines = readFile.readlines()
+    readFile.close()
+    w = open("index.html",'w')
+    w.writelines([item for item in lines[:-1]])
+    w.close()
+
+def cpy_scripts():
+    subprocess.call(['cp', '/Users/liatrio/Documents/Personal-Projects/static-blog/cgi-bin', 'cgi-bin'])
+
+def remove_scripts():
+    subprocess.call(['rm', '-rf', 'cgi-bin/'])
+
+add_button()
+cpy_scripts()
+
+print("Test server booting up:")
+server_proc = subprocess.Popen(['python3', '-m', 'http.server', '--cgi'])
+time.sleep(.5)
+
+def signal_handler(signal, frame):
+        print('shutting down...')
+        server_proc.send_signal(signal.SIGINT)
+
+input("Press any key to stop the server")
+server_proc.send_signal(signal.SIGINT)
+
+trim_button()
+#remove_scripts()
+
+print("Server closed.. have a nice day!")
